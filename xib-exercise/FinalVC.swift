82f90d6a506d33261503e1cf2e1590a743cef2c1
Ryan Collins <ryan@ryanmcollins.com>
2016-03-19 13:05:00 -0400

    Added necessary views and controllers

---
+++ xib-exercise/FinalVC.swift
@@ -0,0 +1,35 @@
+//
+//  FinalVC.swift
+//  xib-exercise
+//
+//  Created by Ryan Collins on 2016-03-19.
+//  Copyright © 2016 Ryan Collins. All rights reserved.
+//
+
+import UIKit
+
+class FinalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
